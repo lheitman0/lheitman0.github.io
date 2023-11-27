659c9b5cea95ff1ff1ddca364ed522c13ea7ec6c
Luke Heitman <lh5938a@student.american.edu>
2023-11-27 18:02:08 -0500

    commit

--- writing/script.js
+++ writing/script.js
@@ -9,13 +9,6 @@ function checkPassword() {
     }
 }
 
-// window.onload = function() {
-//     var url = window.location.href;
-//     if (url.includes('#section2')) {
-//         document.getElementById('passwordPrompt').style.display = 'block';
-//     }
-// }
-
 function showPasswordPrompt() {
     document.getElementById('passwordPrompt').style.display = 'block';
     document.querySelector('.overlay').style.display = 'block';
@@ -32,4 +25,37 @@ window.onload = function() {
         showPasswordPrompt();
     }
 };
+
+// Function to show Luke's writing
+document.getElementById('lukeBtn').addEventListener('click', function() {
+    hideAllContent();
+    document.getElementById('lukeSection').style.display = 'block';
+});
+
+// Function to show the password prompt for Scarlett's section
+document.getElementById('scarlettBtn').addEventListener('click', function() {
+    hideAllContent();
+    document.getElementById('passwordPrompt').style.display = 'block';
+});
+
+// Function to check the password and show Scarlett's writing
+function checkPassword() {
+    var password = document.getElementById('passwordInput').value;
+    if (password === 'password') {
+        document.getElementById('scarlettSection').style.display = 'block';
+        document.getElementById('passwordPrompt').style.display = 'none';
+    } else {
+        alert('Incorrect password');
+    }
+}
+
+// Helper function to hide all content
+function hideAllContent() {
+    var contentSections = document.querySelectorAll('.content');
+    contentSections.forEach(function(section) {
+        section.style.display = 'none';
+    });
+    document.getElementById('passwordPrompt').style.display = 'none';
+}
+
 
